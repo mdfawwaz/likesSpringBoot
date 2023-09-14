728c5ca453d2048790116709add5adcdd8c2556e
mdfawwaz <89119549+mdfawwaz@users.noreply.github.com>
2023-09-14 21:18:34 +0530

    Add files via upload

---
+++ Likes-main/learning-spring/src/main/java/com/prodapt/learningspring/entity/LikeRecord.java
@@ -0,0 +1,18 @@
+package com.prodapt.learningspring.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class LikeRecord {
+
+	@EmbeddedId
+	private LikeId likeId;
+
+}
---
+++ Likes-main/learning-spring/src/main/java/com/prodapt/learningspring/repository/CommentRepository.java
@@ -0,0 +1,5 @@
+package com.prodapt.learningspring.repository;
+
+public class CommentRepository {
+
+}
